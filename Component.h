5a62c82627a9e8c9dea7c08236a7a30e1d1dd246
Sebastian Georghiou <Joxno@users.noreply.github.com>
2014-10-24 18:31:36 +0200

    String comparison for GetComponent

--- Component.h
+++ Component.h
@@ -1,21 +1,23 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
-#include <typeinfo>
 using namespace std;
-class Component
-{
-	private:
-		string m_Name = "";
-	public:
-		Component(string Name) {m_Name=Name;}
-		virtual void Update() {}		// Every Frame
-		virtual void FixedUpdate() {}	// Fixed Interval
-		virtual void Initialize() {}	// Called when added to Handler
+
+	class Component
+	{
+		private:
+			string m_Name = "";
+		public:
+			Component() {}
+			Component(string Name) {m_Name=Name;}
+			virtual void Update() {}		// Every Frame
+			virtual void FixedUpdate() {}	// Fixed Interval
+			virtual void Initialize() {}	// Called when added to Handler
 		
-		virtual string GetName() {return m_Name;}
-		virtual bool isComponent(Component *Comp) { return (typeid(Component) == typeid(*Comp)); }
+			string GetName() {return m_Name;}
+			void SetName(string Name) { m_Name = Name; }
+			virtual bool isComponent(Component *Comp) { return !strcmp(Comp->GetName().c_str(), GetName().c_str()); }
 		
-};
+	};
 
 
 #endif
--- ComponentHandler.h
+++ ComponentHandler.h
@@ -3,16 +3,20 @@
 #include <vector>
 #include "Component.h"
 using namespace std;
-class ComponentHandler
-{
-	private:
-		vector<Component*> m_Components;
-	public:
-		void AddComponent(Component *C) {m_Components.push_back(C); m_Components[m_Components.size()-1]->Initialize();}
-		template<typename T> T* GetComponent() {for(auto C : m_Components) if(typeid(C) == typeid(T)) return C; return null;}
-		template<typename T> void RemoveComponent(T *Component) {int RemoveIndex = 0; for(int i = 0; i < m_Components.size(); i++) if(C->isComponent(Component)) {RemoveIndex = i; break;} m_Components.remove(RemoveIndex); }
-		void UpdateComponents() {for(auto C : m_Components) C->Update();}
-		void FixedUpdateComponents() {for(auto C : m_Components) C->FixedUpdate();}
-};
+
+#define CreateComponent(X) __CreateComponent<X>(#X)
+#define GetComponent(X) __GetComponent<X>(#X)
+	class ComponentHandler
+	{
+		private:
+			vector<Component*> m_Components;
+		public:
+			template<typename T> T* __CreateComponent(string Name) { T *C = new T(); C->Initialize(); m_Components.push_back(C); return C; }
+			void AddComponent(Component *C) {m_Components.push_back(C); m_Components[m_Components.size()-1]->Initialize();}
+			template<typename T> T* __GetComponent(string Name){for (auto C : m_Components){if (!strcmp(Name.c_str(),C->GetName().c_str()))return (T*)C;}return NULL;}
+			template<typename T> void RemoveComponent(T *Component) {int RemoveIndex = 0; for(int i = 0; i < m_Components.size(); i++) if(C->isComponent(Component)) {RemoveIndex = i; break;} m_Components.remove(RemoveIndex); }
+			void UpdateComponents() {for(auto C : m_Components) C->Update();}
+			void FixedUpdateComponents() {for(auto C : m_Components) C->FixedUpdate();}
+	};
 
 #endif
